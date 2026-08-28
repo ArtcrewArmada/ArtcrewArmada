4e6cf02ea81f79e945c08d94608b9be228a9d746
Den Master Fa - capvision@gmail.com <dencapvision@gmail.com>
2026-08-29 01:38:50 +0700

    fix: resolve getLoadContext is not a function runtime error in Pages

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,10 @@
 import { createPagesFunctionHandler } from "@react-router/cloudflare";
+import { RouterContextProvider } from "react-router";
 
 // @ts-ignore - The build artifacts will be generated in CI during the build phase
 import * as build from "../build/server/index.js";
 
-export const onRequest = createPagesFunctionHandler({ build });
+export const onRequest = createPagesFunctionHandler({
+  build,
+  getLoadContext: () => new RouterContextProvider(),
+});
